2b593fb345253fd332f9e9d2c5202f6f029344d4
ElGanzo <lorenzobensong@gmail.com>
2022-06-01 09:39:21 +0200

    TODO: rimane su loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from "src/app/hero";
+import { Component } from '@angular/core';
 
 @Component({
-  // metadata properties:
-
-  //The component's CSS element selector. Matches the name of the HTML element that identifies this component within a parent component's template
-  selector: 'my-app',
-
-  // The location of the component's template file.
+  selector: 'app-root',
   templateUrl: './app.component.html',
-
-  // The location of the component's private CSS styles.
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-
-  title = 'titolo';
-
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+export class AppComponent {
+  title = 'Tour of Heroes';
 }
